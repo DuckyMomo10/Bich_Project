b5ae8e77de2e2a8d678bb9dd271a669f6fbef5a3
DuckyMomo10 <duy33117@gmail.com>
2025-06-06 20:02:47 +0700

    udpate authMiddleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/user'); // đổi tên thành User để tránh trùng biến
+import jwt from 'jsonwebtoken';
+import User from '../models/user.js';
 
-const protect = async (req, res, next) => {
+export const protect = async (req, res, next) => {
   try {
     let token = req.headers.authorization;
 
@@ -9,13 +9,10 @@ const protect = async (req, res, next) => {
       return res.status(401).json({ message: 'Not authorized' });
     }
 
-    token = token.split(' ')[1]; // Lấy token sau "Bearer "
+    token = token.split(' ')[1];
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("Decoded Token: ", decoded);
-
     const user = await User.findById(decoded.id).select('-password');
-    console.log("User from DB: ", user);
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
@@ -28,12 +25,9 @@ const protect = async (req, res, next) => {
   }
 };
 
-// Middleware cho admin-only
-const adminOnly = (req, res, next) => {
+export const adminOnly = (req, res, next) => {
   if (!req.user || req.user.role !== 'admin') {
     return res.status(401).json({ message: 'Not authorized' });
   }
   next();
 };
-
-module.exports = { protect, adminOnly };
